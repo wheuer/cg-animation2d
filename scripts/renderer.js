1a376441275975b3a12f676c540b68038cdc3be9
Cesar Gonzalez <gonz9682@stthomas.edu>
2024-03-06 21:18:26 -0600

    Attempt at bouncing ball. Currently goes into the bottom menu and seems to disapear if you leave the tab.

--- scripts/renderer.js
+++ scripts/renderer.js
@@ -1,4 +1,5 @@
 import * as CG from './transforms.js';
+import { Matrix } from './matrix.js';
 
 class Renderer {
     // canvas:              object ({id: __, width: __, height: __})
@@ -19,13 +20,22 @@ class Renderer {
             slide0: [
                 // example model (diamond) -> should be replaced with actual model
                 {
-                    vertices: [
-                        CG.Vector3(400, 150, 1),
-                        CG.Vector3(500, 300, 1),
-                        CG.Vector3(400, 450, 1),
-                        CG.Vector3(300, 300, 1)
-                    ],
-                    transform: null
+                    // vertices: [
+                    //     CG.Vector3(400, 150, 1),
+                    //     CG.Vector3(500, 300, 1),
+                    //     CG.Vector3(400, 450, 1),
+                    //     CG.Vector3(300, 300, 1)
+                    // ],
+
+                    position: { x: canvas.width / 2, y: canvas.height / 2 }, // Initial position at the center of the canvas
+                    velocity: { x: 300, y: 150 }, // Initial velocity
+
+                    radius: 20,
+                    color: [255, 0, 0, 255], // Red
+
+                    // Initialize prev_time
+                    prev_time: null,
+                    transform: new Matrix(3, 3) // Initialize with an identity matrix
                 }
             ],
             slide1: [],
@@ -87,7 +97,7 @@ class Renderer {
     updateTransforms(time, delta_time) {
         // TODO: update any transformations needed for animation
     }
-    
+
     //
     drawSlide() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -110,21 +120,81 @@ class Renderer {
 
     //
     drawSlide0() {
-        // TODO: draw bouncing ball (circle that changes direction whenever it hits an edge)
-        
-        
+        const ball = this.models.slide0[0];
+
+        // Function to update the position of the ball using transformation matrices
+        const updateBallPosition = (deltaTime) => {
+            // Get the current position vector
+            let currentPosition = new CG.Vector3(ball.position.x, ball.position.y, 1);
+
+            // Define the translation matrix based on velocity and time
+            let translationMatrix = new Matrix(3, 3);
+            CG.mat3x3Translate(translationMatrix, this.models.slide0[0].velocity.x * deltaTime, this.models.slide0[0].velocity.y * deltaTime);
+
+            // Apply translation to the current position vector
+            let newPosition = Matrix.multiply([translationMatrix, currentPosition]);
+
+            // Update the position of the ball
+            this.models.slide0[0].position.x = newPosition.values[0][0];
+            this.models.slide0[0].position.y = newPosition.values[1][0];
+
+            // Check for collisions with canvas edges and adjust position if necessary
+            handleCanvasEdgesCollision();
+        };
+
+        // Function to handle collisions with canvas edges
+        const handleCanvasEdgesCollision = () => {
+            if (ball.position.x <= ball.radius || ball.position.x >= this.canvas.width - ball.radius) {
+                ball.velocity.x *= -1;
+
+            }
+            if (ball.position.y <= ball.radius || ball.position.y >= this.canvas.height - ball.radius) {
+                ball.velocity.y *= -1;
+
+            }
+        };
+
+        // Function to draw the ball on the canvas
+        const drawBall = () => {
+            this.ctx.fillStyle = `rgba(${ball.color})`;
+            this.ctx.beginPath();
+            this.ctx.arc(ball.position.x, ball.position.y, ball.radius, 0, Math.PI * 2);
+            this.ctx.fill();
+        };
+
+        // Function to animate the bouncing ball
+        const animateBall = (timestamp) => {
+            const deltaTime = (timestamp - this.models.slide0[0].prev_time) / 1000;
+
+            // Update ball position using transformation matrices
+            updateBallPosition(deltaTime);
+
+            // Clear the canvas and draw the ball
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            drawBall();
+
+            // Request next animation frame
+            requestAnimationFrame(animateBall);
+
+            // Update lastTimestamp
+            this.models.slide0[0].prev_time = timestamp;
+        };
+
+        // Start the animation
+        animateBall(this.models.slide0[0].prev_time);
+
         // Following lines are example of drawing a single polygon
         // (this should be removed/edited after you implement the slide)
-        let teal = [0, 128, 128, 255];
-        this.drawConvexPolygon(this.models.slide0[0].vertices, teal);
+        // let teal = [0, 128, 128, 255];
+        // this.drawConvexPolygon(this.models.slide0[0].vertices, teal);
     }
 
     //
     drawSlide1() {
         // TODO: draw at least 3 polygons that spin about their own centers
         //   - have each polygon spin at a different speed / direction
-        
-        
+
+
     }
 
     //
@@ -141,10 +211,10 @@ class Renderer {
         // TODO: get creative!
         //   - animation should involve all three basic transformation types
         //     (translation, scaling, and rotation)
-        
-        
+
+
     }
-    
+
     // vertex_list:  array of object [Matrix(3, 1), Matrix(3, 1), ..., Matrix(3, 1)]
     // color:        array of int [R, G, B, A]
     drawConvexPolygon(vertex_list, color) {
--- scripts/transforms.js
+++ scripts/transforms.js
@@ -6,23 +6,35 @@ import { Matrix } from "./matrix.js";
 // Set values of existing 3x3 matrix to the identity matrix
 function mat3x3Identity(mat3x3) {
     mat3x3.values = [[1, 0, 0],
-                     [0, 1, 0],
-                     [0, 0, 1]];
+    [0, 1, 0],
+    [0, 0, 1]];
 }
 
 // Set values of existing 3x3 matrix to the translate matrix
 function mat3x3Translate(mat3x3, tx, ty) {
-    // mat3x3.values = ...;
+    mat3x3.values = [
+        [1, 0, tx],
+        [0, 1, ty],
+        [0, 0, 1]
+    ];
 }
 
 // Set values of existing 3x3 matrix to the scale matrix
 function mat3x3Scale(mat3x3, sx, sy) {
-    // mat3x3.values = ...;
+    mat3x3.values = [
+        [sx, 0, 0],
+        [0, sy, 0],
+        [0, 0, 1]
+    ];
 }
 
 // Set values of existing 3x3 matrix to the rotate matrix
 function mat3x3Rotate(mat3x3, theta) {
-    // mat3x3.values = ...;
+    mat3x3.values = [
+        [Math.cos(theta), -(Math.sin(theta)), 0],
+        [Math.sin(theta), Math.cos(theta), 0],
+        [0, 0, 1]
+    ];
 }
 
 // Create a new 3-component vector with values x,y,w
